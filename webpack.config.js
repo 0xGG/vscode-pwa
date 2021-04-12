727b37a7da4592073191804271de7324e51fd607
Yiyi Wang <shd101wyy@gmail.com>
2021-04-13 00:04:19 +0800

    feat: Initial release

--- webpack.config.js
+++ webpack.config.js
@@ -32,14 +32,12 @@ const config = {
       template: "./index.html",
       filename: path.resolve(__dirname, "./dist/index.html"),
     }),
-    /*
     new WorkboxPlugin.GenerateSW({
       // these options encourage the ServiceWorkers to get in there fast
       // and not allow any straggling "old" SWs to hang around
       clientsClaim: true,
       skipWaiting: true,
     }),
-    */
     new CopyWebpackPlugin({
       patterns: [
         {
